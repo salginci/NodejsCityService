3aa966a9c46188fde2fcf6a4709135b9e12c89d5
salginci <ozgur@salginci.com>
2022-02-13 23:33:18 +0300

    db config updated

--- db.js
+++ db.js
@@ -6,8 +6,8 @@ var pool =
   mariadb.createPool({
     host: "127.0.0.1", 
     port: 3306,
-    user: "root", 
-    password: "0440",
+    user: "sigal", 
+    password: "8hRE+zpyLk",
     database: "world"
   });
  
