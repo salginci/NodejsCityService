afe4229792a5ba0e06229eb04a3b1d533c7c0d29
salginci <ozgur@salginci.com>
2022-02-13 23:44:46 +0300

    connection string updated

--- cities-webhook.sh
+++ cities-webhook.sh
@@ -9,5 +9,5 @@ docker rm $(docker stop $(docker ps -a -q --filter ancestor=cityservice --format
 docker rmi -f cityservice
 echo "remove existing container"
 docker build -t cityservice .
-docker run -d -p  9051: 8088 --name cityservice cityservice
+docker run -d -p  9051:8088 --name cityservice cityservice
 echo "City service is assigned to port 9051"
